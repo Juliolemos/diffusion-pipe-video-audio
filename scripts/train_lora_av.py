941ff1b97b009e9a090a841b43b3860bab40c56e
Julio Lemos <old.mores@gmail.com>
2025-06-14 13:38:57 +0100

    Create train_lora_av.py

---
+++ scripts/train_lora_av.py
@@ -0,0 +1,22 @@
+import json
+import torch
+from torch.utils.data import DataLoader
+from diffusion_pipe.datasets.audio_video_dataset import AudioVideoDataset
+from diffusion_pipe.pipelines.wan21 import Wan21Pipeline
+from diffusion_pipe.trainers.lora_av_trainer import LoRAAVTrainer
+from accelerate import Accelerator
+
+def main():
+    accelerator = Accelerator()
+    manifest = json.load(open("annotations/av_manifest.json"))
+    dataset = AudioVideoDataset(manifest)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+    model = Wan21Pipeline.from_pretrained("tdrussell/wang2.1").cuda()
+    trainer = LoRAAVTrainer(model, dataloader, accelerator)
+    trainer.train(num_epochs=10)
+
+    model.save_pretrained("output/lora-av-model")
+
+if __name__ == "__main__":
+    main()
